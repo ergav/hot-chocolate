3dc4093659249d185d0bb5350de593428dc9800b
ergav <erik.gafvelin@adelfors.nu>
2021-09-29 14:59:23 +0200

    Small adjustmets

--- Assets/Scripts/MouseHover.cs
+++ Assets/Scripts/MouseHover.cs
@@ -9,25 +9,13 @@ public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 {
     public UnityEvent OnMouseEnter, onMouseExit;
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
-        UnityEngine.Debug.Log("OnEnter");
         OnMouseEnter.Invoke();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        UnityEngine.Debug.Log("OnExit");
         onMouseExit.Invoke();
     }
 }
