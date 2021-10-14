c464f0ddedcb56b87590d4e1ad4032fc2e9dd2c3
ergav <erik.gafvelin@adelfors.nu>
2021-10-14 14:57:59 +0200

    Star UI

--- Assets/Scripts/RotatingObject.cs
+++ Assets/Scripts/RotatingObject.cs
@@ -6,6 +6,10 @@ public class RotatingObject : MonoBehaviour
 {
     public Vector3 rotateSpeed = new Vector3(100, 100, 100);
 
+    Vector3 speedRotate;
+
+    int dirRNGX, dirRNGY, dirRNGZ;
+
     FallingObject fallingObject;
 
     void Start()
@@ -18,13 +22,36 @@ public class RotatingObject : MonoBehaviour
                 fallingObject = GetComponentInParent<FallingObject>();
             }
         }
+
+        speedRotate = new Vector3(Random.Range(rotateSpeed.x - (rotateSpeed.x * 0.8f), rotateSpeed.x - (rotateSpeed.x * 1.2f)), Random.Range(rotateSpeed.y - (rotateSpeed.y * 0.8f), rotateSpeed.y - (rotateSpeed.y * 1.2f)), Random.Range(rotateSpeed.z - (rotateSpeed.z * 0.8f), rotateSpeed.z - (rotateSpeed.z * 1.2f)));
+
+        dirRNGX = Random.Range(0, 1);
+        dirRNGY = Random.Range(0, 1);
+        dirRNGZ = Random.Range(0, 1);
+
+
+        if (dirRNGX == 1)
+        {
+            speedRotate.x = -speedRotate.x;
+        }
+
+        if (dirRNGY == 1)
+        {
+            speedRotate.y = -speedRotate.y;
+        }
+
+        if (dirRNGZ == 1)
+        {
+            speedRotate.z = -speedRotate.z;
+        }
+
     }
 
     void Update()
     {
         if (fallingObject.falling)
         {
-            transform.Rotate(rotateSpeed.x * Time.deltaTime, rotateSpeed.y * Time.deltaTime, rotateSpeed.z * Time.deltaTime);
+            transform.Rotate(speedRotate.x * Time.deltaTime, speedRotate.y * Time.deltaTime, speedRotate.z * Time.deltaTime);
 
         }
     }
