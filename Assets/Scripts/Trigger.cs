48c0a7d436485e81ca25f0f4af6eb540d9834166
ergav <erik.gafvelin@adelfors.nu>
2021-11-26 10:56:20 +0100

    Functioning toaster and refined trigger script

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -9,15 +9,29 @@ public class Trigger : MonoBehaviour
     public UnityEvent triggerEvent;
     public UnityEvent triggerExitEvent;
 
+    [SerializeField] bool onceOnly;
+
+    [SerializeField] float delay = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        triggerEvent.Invoke();
+        StartCoroutine(TriggerEnterDelay());
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         triggerExitEvent.Invoke();
+        if (onceOnly)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    IEnumerator TriggerEnterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        triggerEvent.Invoke();
 
     }
 
