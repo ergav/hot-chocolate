23bf20364d629a7a740c931a582559a81b5ac8af
ergav <erik.gafvelin@adelfors.nu>
2021-12-16 10:52:58 +0100

    Credits skippable and takes player back to menu

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Credits : MonoBehaviour
+{
+    public GameObject creditsText;
+    Animator anim;
+
+    void Start()
+    {
+        anim = creditsText.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("End"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
